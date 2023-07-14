e65e7e9aa576315070920e0a083481e3fb59c3a2
Raniaell <rania.elloumi2@gmail.com>
2023-07-14 16:18:52 +0100

    .

--- covid_19.py
+++ covid_19.py
@@ -11,5 +11,5 @@ st.write("The chosen number is 👉:", x)
 lg.coef_=load("y.joblib")
 lg.intercept_=load("y2.joblib")
 y = lambda z : lg.coef_[3]*x**3 + lg.coef_[2]*x**2 +lg.coef_[1]*x + lg.coef_[0] * lg.intercept_
-if (st.button("Deaths predictor")):
+if (st.button("💀 Deaths predictor 💀")):
     st.write(round(y(x)))
