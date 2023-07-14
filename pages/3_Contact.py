efc27e3fc2642eb3d95d7e91f3507c5ebd9e68fc
Raniaell <rania.elloumi2@gmail.com>
2023-07-14 17:47:13 +0100

    update

--- pages/3_Contact.py
+++ pages/3_Contact.py
@@ -1,3 +1,7 @@
 import streamlit as st 
 
 st.title("Contact")
+if st.button("Check my email here 👇"):
+    st.write("✉️ E-mail:  rania.elloumi2@gmail.com")
+if st.button("Check my phone number here 👇"):
+    st.write("📲 phone number: +216 ********")
