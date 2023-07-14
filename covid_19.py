b5e2a649340b97a53feff256e647ac04e5bd88bc
Raniaell <rania.elloumi2@gmail.com>
2023-07-14 11:32:31 +0100

    update

--- covid_19.py
+++ covid_19.py
@@ -3,9 +3,10 @@ from joblib import load
 st.title("Welcome to the Covid 19 data of the United Kingdom UK")
 st.header("Let's start")
 lg = load('covid19.joblib')
-x = st.text_input("The number of cases confirmed is:")
+x = int(st.text_input("The number of cases confirmed is:"))
+st.write("The chosen number is :", x)
 lg.coef_=load("y.joblib")
 lg.intercept_=load("y2.joblib")
-y = lambda x : lg.coef_[1]*pow(x,3) + lg.coef_[2]*pow(x,2) +lg.coef_[3]*x + lg.intercept_
+y = lambda z : lg.coef_[3]*x**3 + lg.coef_[2]*x**2 +lg.coef_[1]*x + lg.coef_[0] * lg.intercept_
 if (st.button("Deaths predictor")):
     st.write(round(y(x)))
