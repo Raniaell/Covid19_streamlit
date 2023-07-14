61a674cac248da185aa1cbdd4edef64b6950f4c9
Raniaell <rania.elloumi2@gmail.com>
2023-07-14 16:16:29 +0100

    .

--- covid_19.py
+++ covid_19.py
@@ -7,7 +7,7 @@ st.sidebar.success("Select a page above")
 st.header("Let's start")
 lg = load('covid19.joblib')
 x = st.number_input("The number of cases confirmed is:")
-st.write("The chosen number is :", x)
+st.write("The chosen number is 👉:", x)
 lg.coef_=load("y.joblib")
 lg.intercept_=load("y2.joblib")
 y = lambda z : lg.coef_[3]*x**3 + lg.coef_[2]*x**2 +lg.coef_[1]*x + lg.coef_[0] * lg.intercept_
