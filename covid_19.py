6182312ed467467b9897e62025c1a48b20cbb4ba
Raniaell <rania.elloumi2@gmail.com>
2023-07-14 11:28:02 +0100

    covid19 update

--- covid_19.py
+++ covid_19.py
@@ -3,7 +3,7 @@ from joblib import load
 st.title("Welcome to the Covid 19 data of the United Kingdom UK")
 st.header("Let's start")
 lg = load('covid19.joblib')
-x = int(st.text_input("The number of cases confirmed is:"))
+x = st.text_input("The number of cases confirmed is:")
 lg.coef_=load("y.joblib")
 lg.intercept_=load("y2.joblib")
 y = lambda x : lg.coef_[1]*pow(x,3) + lg.coef_[2]*pow(x,2) +lg.coef_[3]*x + lg.intercept_
